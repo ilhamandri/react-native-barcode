a658eae3abcf2d5c692bb5b3e023ce6cce07e927
ilhamandri <ilhamandriandri@gmail.com>
2020-04-14 18:53:10 +0700

    add react-navigation

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-/**
- * @format
- */
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import BarcodeGenerator from './src/BarcodeGenerator';
+import Routes from './src/Routes';
 
-AppRegistry.registerComponent(appName, () => BarcodeGenerator);
+AppRegistry.registerComponent(appName, () => Routes);
